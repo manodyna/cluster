422b079e573bfcd860c7f6bba94a9289fba3f6bd
Manodyna K H <77208955+manodyna@users.noreply.github.com>
2022-06-20 10:09:29 +0530

    Update controlPlane.sh

--- controlPlane.sh
+++ controlPlane.sh
@@ -2,4 +2,9 @@
 sudo rm /etc/containerd/config.toml
 sudo systemctl restart containerd
 kubeadm init 
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl taint nodes --all node-role.kubernetes.io/master-
+kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
 
