a021a074ceec77c31d78890fc744cd36a41fe187
Manodyna K H <77208955+manodyna@users.noreply.github.com>
2022-06-21 10:54:49 +0530

    Update join.sh

--- join.sh
+++ join.sh
@@ -1,3 +1,4 @@
-!#/bin/bash
-kubeadm join 10.1.10.63:6443 --token ivmvy5.h2ecw5hjoqwgcxbz \
-	--discovery-token-ca-cert-hash sha256:21293b03866466c74ee951b8e905357f36c58c3e0397b4657aaf84b782d9ddd7 
+#!/bin/bash
+kubeadm join 10.1.10.65:6443 --token tfrff6.3uvyi6csupkxsy6m \
+	--discovery-token-ca-cert-hash sha256:beac2f7d0b6290268e7676d0b00c84451bbdd5f3293425fed1bd1efc52bf5594  \
+	--node-name node01
