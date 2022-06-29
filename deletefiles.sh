86667f7c1cd652bc968db53dfff1e680cfab1864
Manodyna K H <77208955+manodyna@users.noreply.github.com>
2022-06-29 13:04:45 +0530

    Add files via upload

---
+++ deletefiles.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo rm /etc/kubernetes/manifests/kube-controller-manager.yaml
+sudo rm /etc/kubernetes/manifests/kube-scheduler.yaml
+sudo rm /etc/kubernetes/manifests/kube-apiserver.yaml
+sudo rm /etc/kubernetes/manifests/etcd.yaml
