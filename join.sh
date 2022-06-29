571595e1e7e8be4a5f4cffd7c5be7aa3ec2a53c7
Manodyna K H <77208955+manodyna@users.noreply.github.com>
2022-06-29 12:04:48 +0530

    Add files via upload

--- join.sh
+++ join.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-sudo rm /etc/containerd/config.toml
-sudo systemctl restart containerd
-kubeadm join 10.1.10.65:6443 --token tfrff6.3uvyi6csupkxsy6m \
-	--discovery-token-ca-cert-hash sha256:beac2f7d0b6290268e7676d0b00c84451bbdd5f3293425fed1bd1efc52bf5594  \
+kubeadm join 10.1.10.63:6443 --token f04vj7.7jd3bvwfq2u321an \
+	--discovery-token-ca-cert-hash sha256:21b7e786c4b5b0d835ad027809268800c631158bab3e2fccdc9e1e42daca691c \
 	--node-name node01
+
