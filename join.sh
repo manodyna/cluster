a411ea4fa58ab403c3f47debdec11223939f67df
Manodyna K H <77208955+manodyna@users.noreply.github.com>
2022-06-30 12:56:50 +0530

    Update join.sh

--- join.sh
+++ join.sh
@@ -12,5 +12,5 @@ sudo systemctl start docker
 sudo systemctl enable docker
 sudo kubeadm join 10.1.10.63:6443 --token 85rnqz.zctn8cc47q1w595c \
 	--discovery-token-ca-cert-hash sha256:d78598d8dd58e453e42af262501e8c1ff8cbcd9ce4d1cbb649ef9b4f3b5b307f \
-	--node-name node02
+	--node-name node01
 
