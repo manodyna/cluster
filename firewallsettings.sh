23cc766bbf2f9aee8fb7b9e617386fda2e52b6ce
Manodyna K H <77208955+manodyna@users.noreply.github.com>
2022-07-01 12:29:42 +0530

    Update firewallsettings.sh

--- firewallsettings.sh
+++ firewallsettings.sh
@@ -1,8 +1,6 @@
 sudo apt install firewalld
-firewall-cmd --permanent --add-port=4789/udp 
-kubectl get pods -n calico-system
-firewall-cmd --permanent --add-port=5473/tcp 
-sudo firewall-cmd --permanent --add-port=179/tcp 
-kubectl get pods -n calico-system
+sudo firewall-cmd --permanent --add-port=4789/udp 
+sudo firewall-cmd --permanent --add-port=5473/tcp 
+sudo firewall-cmd --permanent --add-port=179/tcp
+sudo firewall-cmd --permanent --add-port=179/tcp --zone=docker
 sudo firewall-cmd --reload
-
